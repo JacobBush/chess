393aa94ebf793e92d1763639c97a3f0e34e7feee
Jacob Bush <jbush@uwaterloo.ca>
2017-07-28 17:07:23 -0400

    Rook movement

--- src/Rook.java
+++ src/Rook.java
@@ -1,5 +1,6 @@
 import java.awt.Point;
 import java.util.List;
+import java.util.ArrayList;
 
 public class Rook extends Piece {	
     public Rook (Point location, Piece.Color color, Game game) {
@@ -11,7 +12,46 @@ public class Rook extends Piece {
     }
 
     public List<Point> getValidMoves() {
-    	return null;
+    	List<Point> validMoves = new ArrayList<Point>();
+    	Point p = this.getLocation();
+    	
+    	for (int y = p.y + 1; y < Game.BOARD_SIZE; y ++) { // up
+    		Point pp = new Point(p.x, y);
+    		if (checkValidity(pp)) validMoves.add(pp);
+    		if (checkBreak(pp)) break;
+    	}
+    	for (int y = p.y - 1; y >= 0; y --) { // down
+    		Point pp = new Point(p.x, y);
+    		if (checkValidity(pp)) validMoves.add(pp);
+    		if (checkBreak(pp)) break;
+    	}
+    	for (int x = p.x + 1; x < Game.BOARD_SIZE; x ++) { // right
+    		Point pp = new Point(x, p.y);
+    		if (checkValidity(pp)) validMoves.add(pp);
+    		if (checkBreak(pp)) break;
+    	}
+    	for (int x = p.x - 1; x >= 0; x --) { // left
+    		Point pp = new Point(x, p.y);
+    		if (checkValidity(pp)) validMoves.add(pp);
+    		if (checkBreak(pp)) break;
+    	}
+    	
+    	return validMoves;
+    }
+    
+    private boolean checkValidity (Point p) {
+    	if (Game.validPoint (p)) {
+			Piece piece = this.getGame().getPieceAt(p);
+			return isEmpty(piece) || isEnemy(piece);
+    	}
+    	return false;
+    }
+    private boolean checkBreak (Point p) {
+    	if (Game.validPoint (p)) {
+    		Piece piece = this.getGame().getPieceAt(p);
+			return isAlly(piece) || isEnemy(piece);
+    	}
+    	return false;
     }
     // usability
     @Override
